55400e2d81abf3fbaf9b89f7db90cffa3dba3523
Garik <garik.harutyunian@gmail.com>
2023-04-09 20:14:49 +0400

    42/Keep progress in redux

--- src/pages/level/Level.tsx
+++ src/pages/level/Level.tsx
@@ -4,12 +4,13 @@ import {Colors} from '../../style';
 import {Matrix} from '../components';
 import {NumberBoard} from '../components/NumberBoard';
 import {useDispatch, useSelector} from 'react-redux';
-import {checkIsSolved, getLevel} from '../../store/levelSlice';
+import {checkIsSolved, getLevel, selectLevel} from '../../store/levelSlice';
 import {TopBar} from '../components/TopBar';
 import {Win} from '../components/Win';
 import {RouteProp} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {Screen} from '../components/Screen';
+import {changeLastAvailableLevel} from '../../store/userSlice';
 
 type RootStackParamList = {
   Level: {id: string};
@@ -30,10 +31,20 @@ const Level = (props: ILevelProps) => {
   const {navigation, route} = props;
   const dispatch = useDispatch<any>();
   const isSolved = useSelector(checkIsSolved);
+  const activeLevel = useSelector(
+    selectLevel,
+    (level1, level2) => level1?.id === level2?.id
+  );
 
   useEffect(() => {
     dispatch(getLevel(route.params.id));
   }, []);
+
+  useEffect(() => {
+    if (isSolved) {
+      dispatch(changeLastAvailableLevel(activeLevel?.index || 0));
+    }
+  }, [isSolved]);
 
   const onBack = () => navigation.goBack();
 
